e8be08a4db00b4785e152924fb637b9aeaef62ad
Javier Castiarena <jcastiarena@mail.utdt.edu>
2024-09-19 14:55:09 -0300

    Added cors support to client

--- api/hello.js
+++ api/hello.js
@@ -1,10 +1,20 @@
 import express from 'express';
+import cors from 'cors';
+
 const app = express();
+
+// Allow localhost:3000 for CORS
+const corsOptions = {
+  origin: 'http://localhost:5173', // Your client origin
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true, // Allow cookies if needed
+}
 
 import userController from '../controllers/userController.js';
 import { validateUser, handleValidationErrors } from '../middlewares/userValidation.js';
 
 app.use(express.json());
+app.use(cors(corsOptions));
 
 app.post('/api/users', validateUser, handleValidationErrors, userController.createUser);
 app.get('/api/users', userController.getAllUsers);
